7983f0d56e4281ab0755cb0c19eff607e7aa6fba
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-25 16:11:20 +0200

    id

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ app.get('/', (req, res) => {
   res.send('Hello World! It\'s Codecool!')
 });
 
-app.get('/students', (req, res) => { //a megadott json filet akarjuk vele beolvastatni a bongeszoben
+app.get('/api/students', (req, res) => { //a megadott json filet akarjuk vele beolvastatni a bongeszoben
     const file = __dirname + "/data/students.json";
     fs.readFile(file, (err, data) => {
         if (err) throw err;
         res.setHeader("content-type", "application/json");
         res.send(data);
-      });
+      });      
+});
+
+app.get('/api/students/:id', (req, res) => {
+  const id = req.params.id;
+  const file = __dirname + "/data/students.json";
+  fs.readFile(file, (err, data) => {
+    if (err) throw err;
+    const students = JSON.parse(data); //creating object
+    res.setHeader("content-type", "application/json");
+    const student = students.find(student => student.id == id); 
+    console.log(students[0]);
+    console.log(typeof id);
+
+    res.send(student);
+  }); 
 });
 
 app.listen(port, () => {
