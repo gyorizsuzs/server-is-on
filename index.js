090a06b0f63fcbd2f1560e1ed9ea459441fb1c31
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-29 01:13:41 +0200

    post newstudent + refactoring

--- index.js
+++ index.js
@@ -1,36 +1,101 @@
-const express = require('express');
+// MODULES
+
+const express = require("express");
 const app = express();
 const fs = require("fs"); //FILE SYSTEM
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World! It\'s Codecool!')
-});
+app.use(express.json());
 
-app.get('/api/students', (req, res) => { //a megadott json filet akarjuk vele beolvastatni a bongeszoben
-    const file = __dirname + "/data/students.json";
-    fs.readFile(file, (err, data) => {
-        if (err) throw err;
-        res.setHeader("content-type", "application/json");
-        res.send(data);
-      });      
-});
+// ROUTES
 
-app.get('/api/students/:id', (req, res) => {
+const students = JSON.parse(fs.readFileSync(`${__dirname}/data/students.json`));
+
+const getAllStudents = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: students.length,
+    data: {
+      students,
+    },
+  });
+};
+
+const getStudent = (req, res) => {
+  /*   const file = __dirname + "/data/students.json";
+  fs.readFile(file, (err, data) => {
+    if (err) throw err;
+    res.setHeader("content-type", "application/json");
+    res.send(data);
+  });
+}; */
+  res.status(200).json({
+    status: "success",
+    results: students.length,
+    data: {
+      students,
+    },
+  });
+};
+
+const getId = (req, res) => {
   const id = req.params.id;
   const file = __dirname + "/data/students.json";
   fs.readFile(file, (err, data) => {
     if (err) throw err;
     const students = JSON.parse(data); //creating object
     res.setHeader("content-type", "application/json");
-    const student = students.find(student => student.id == id); 
-    console.log(students[0]);
-    console.log(typeof id);
+    const student = students.find((student) => student.id == id);
+    /*  console.log(students[0]);
+    console.log(typeof id); */
 
     res.send(student);
-  }); 
-});
+  });
+};
+
+const getStatus = (req, res) => {
+  const status = req.params.status;
+  const active = students.find((el) => el.status === true);
+  const finished = students.find((el) => el.status === false);
+
+  fs.readFile(file, (err, data) => {
+    if (err) throw err;
+    const students = JSON.parse(data);
+    res.setHeader("content-type", "application/json");
+    const student = students.find((student) => student.status == status);
+
+    res.send(student);
+  });
+};
+
+const createStudent = (req, res) => {
+  const newStudent = students[students.lenght + 1];
+  /* const newStudent = Object.assign({ id: newId }, req.body); */
+
+  students.push(newStudent);
+
+  fs.writeFile(
+    `${__dirname}/data/students.json`,
+    JSON.stringify(students),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          newStudent,
+        },
+      });
+    }
+  );
+};
+
+// HANDLERS
+
+app.get("/", getAllStudents);
+app.get("/api/students", getStudent);
+app.get("/api/students/:id", getId);
+app.get("/api/students/:status", getStatus);
+app.post("/api/students", createStudent);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App is listening on port ${port}`);
 });
